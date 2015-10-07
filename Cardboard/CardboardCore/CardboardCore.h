626a6b03ce67caceee73d7ab90474f156eb9dc25
Jason Patterson <jcpstuff@gmail.com>
2015-10-07 16:42:06 -0400

    Add CardboardCore target

---
+++ Cardboard/CardboardCore/CardboardCore.h
@@ -0,0 +1,19 @@
+//
+//  CardboardCore.h
+//  CardboardCore
+//
+//  Created by PattersonJ on 10/7/15.
+//  Copyright © 2015 Jason Patterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CardboardCore.
+FOUNDATION_EXPORT double CardboardCoreVersionNumber;
+
+//! Project version string for CardboardCore.
+FOUNDATION_EXPORT const unsigned char CardboardCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardboardCore/PublicHeader.h>
+
+
