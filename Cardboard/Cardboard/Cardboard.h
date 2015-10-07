fd279646e59cfd628c9d974dfa8df7ad2232273e
Jason Patterson <jcpstuff@gmail.com>
2015-10-07 16:34:18 -0400

    Add Framework project

---
+++ Cardboard/Cardboard/Cardboard.h
@@ -0,0 +1,19 @@
+//
+//  Cardboard.h
+//  Cardboard
+//
+//  Created by PattersonJ on 10/7/15.
+//  Copyright © 2015 Jason Patterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cardboard.
+FOUNDATION_EXPORT double CardboardVersionNumber;
+
+//! Project version string for Cardboard.
+FOUNDATION_EXPORT const unsigned char CardboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cardboard/PublicHeader.h>
+
+
